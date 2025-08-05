afdb91c1a0bf0dff888aa2ce28e88634060a1de0
unknown <howwel766@gmail.com>
2025-08-05 10:19:38 +0800

    fixed logic for switching & added name for projects

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,33 +1,43 @@
-        const projects = [
-            {
-                img: 'assets/images/portfolio-website.png',
-                alt: 'Portfolio Website',
-                link: 'https://github.com/yourusername/portfolio-website'
-            },
-            {
-                img: 'assets/images/todo-app.png',
-                alt: 'Todo App',
-                link: 'https://github.com/yourusername/todo-app'
-            },
-            {
-                img: 'assets/images/smart-scheduler.png',
-                alt: 'Smart Scheduler',
-                link: 'https://github.com/yourusername/smart-scheduler'
-            }
-        ];
-        let current = 0;
-        const image = document.getElementById('project-image');
-        const link = document.querySelector('.gallery-link');
-        document.querySelector('.gallery-nav.left').onclick = () => {
-            current = (current - 1 + projects.length) % projects.length;
-            updateGallery();
-        };
-        document.querySelector('.gallery-nav.right').onclick = () => {
-            current = (current + 1) % projects.length;
-            updateGallery();
-        };
-        function updateGallery() {
-            image.src = projects[current].img;
-            image.alt = projects[current].alt;
-            link.href = projects[current].link;
-        }
+const projects = [
+    {
+        img: 'assets/images/pfp.jpg',
+        alt: 'Portfolio Website',
+        link: 'https://github.com/yourusername/portfolio-website',
+        title: 'Portfolio Website'
+    },
+    {
+        img: 'assets/images/todo-app.png',
+        alt: 'Todo App',
+        link: 'https://github.com/yourusername/todo-app',
+        title: 'Todo App'
+    },
+    {
+        img: 'assets/images/smart-scheduler.png',
+        alt: 'Smart Scheduler',
+        link: 'https://github.com/yourusername/smart-scheduler',
+        title: 'Smart Scheduler'
+    }
+];
+
+let current = 0;
+const image = document.getElementById('project-image');
+const link = document.querySelector('.gallery-link');
+const title = document.querySelector('.project-title');
+
+document.querySelector('.gallery-nav.left').onclick = () => {
+    current = (current - 1 + projects.length) % projects.length;
+    updateGallery();
+};
+document.querySelector('.gallery-nav.right').onclick = () => {
+    current = (current + 1) % projects.length;
+    updateGallery();
+};
+
+function updateGallery() {
+    image.src = projects[current].img;
+    image.alt = projects[current].alt;
+    link.href = projects[current].link;
+    title.textContent = projects[current].title;
+}
+
+updateGallery();
